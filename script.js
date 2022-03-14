9a8da3afb31925d87ef769c563a5570a89c75e49
GordxRohit <82945265+GordxRohit@users.noreply.github.com>
2022-03-14 19:31:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const upBtn = document.querySelector(".up-button");
+const downBtn = document.querySelector(".down-button");
+const sidebar = document.querySelector(".sidebar");
+const container = document.querySelector(".container");
+const mainSlide = document.querySelector(".main-slide");
+const slideCount = mainSlide.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slideCount - 1) * 100}vh`;
+
+upBtn.addEventListener("click", () => {
+  changeSlide("up");
+});
+
+downBtn.addEventListener("click", () => {
+  changeSlide("down");
+});
+
+function changeSlide(direction) {
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex === slideCount) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slideCount - 1;
+    }
+  }
+  const height = container.clientHeight;
+
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}
+/* Credit and Tnanks: Traversy Media*/
